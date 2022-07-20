ee0d099a536835d3e611b0887be33f41f7dbe7d4
lauradillonar <laura.dillon.ar@gmail.com>
2022-07-20 20:01:30 -0300

    Autenticación básica con usuario y password

---
+++ src/main/java/com/confluenciacreativa/demosecurityjwt/rest/DemoRest.java
@@ -0,0 +1,33 @@
+package com.confluenciacreativa.demosecurityjwt.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("")
+public class DemoRest {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoRest.class);
+
+    @GetMapping("/mensaje")
+    public ResponseEntity<?> getMensaje(){
+
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+
+        logger.info("Datos del Usuario: {}", auth.getPrincipal());
+        logger.info("Datos de los Permisos {}", auth.getAuthorities());
+        logger.info("Está autenticado {}", auth.isAuthenticated());
+
+        Map<String, String> mensaje = new HashMap<>();
+        mensaje.put("contenido", "Hola Mundo");
+        return ResponseEntity.ok(mensaje);
+    }
+}
